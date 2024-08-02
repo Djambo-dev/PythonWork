9a52800014962b462b16861d6d90dc2aaa17ab03
DMSadykov <dzhamil.sadykov@gmail.com>
2024-08-02 11:04:34 +0300

    feature 1.2 added

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import cv2
 
-img = cv2.imread('images/image.jpg')
-cv2.imshow('Result', img)
+# img = cv2.imread('images/image.jpg')
+# cv2.imshow('Result', img)
+#
+# cv2.waitKey(0)
 
-cv2.waitKey(0)
+cap = cv2.VideoCapture('videos/video.mp4')
+
+while True:
+    success, img = cap.read()
+    cv2.imshow('Result', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
