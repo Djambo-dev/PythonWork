c1ef9f0cbe8d886b64e214dbd72286d9f9a6ba30
DMSadykov <dzhamil.sadykov@gmail.com>
2024-08-06 11:42:33 +0300

    Task#1 Feature 1.1

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 import cv2
 
 # Task 2 Listening 1.1
-img = cv2.imread('images/Monkey-Selfie.jpg')
-img = cv2.GaussianBlur(img, (3, 3), 0)
+
+# Source Image
+source = cv2.imread('images/Monkey-Selfie.jpg')
+cv2.imshow('Source Image', source)
+
+
+# Grayscale Image
+gray_image = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+cv2.imshow('Grayscale Image', gray_image)
+
+# Blur Image
+blur_image = cv2.blur(gray_image, [3, 3], )
+
+# Gaussian Image
+gaussian_blur_image = cv2.GaussianBlur(gray_image, (3, 3), 0)
+cv2.imshow('source', gaussian_blur_image)
+cv2.waitKey(0)
+
